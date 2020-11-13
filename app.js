d66ef6338b1298cd94e2529d66bb3c0ce83a932d
shalom dawit <shalom.dawit@gmail.com>
2020-11-13 00:23:50 -0500

    app creates and appends team.html file

--- app.js
+++ app.js
@@ -4,24 +4,33 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
+const render = require("./lib/htmlRenderer");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-//call render func with completed employee array to generate final html as a string
-const render = require("./lib/htmlRenderer");
-const Employee = require("./lib/Employee");
 
+let team = [];
+//console.log("it barely works");
 
+//creates new or erases then creates new team.html file
+const c_Html = function createHTML(member) {
+  team.push(member);
+  fs.writeFileSync(outputPath, render(team), function (err) {
+    if (err) {
+      throw err;
+    }
+  });
+};
+//appends team.html file if already existing (note: also creates new, but does not erase, so this functionality is useless right now)
+const a_Html = function addToHTML(member) {
+  team.push(member);
+  fs.appendFileSync(outputPath, render(team), function (err) {
+    if (err) {
+      throw err;
+    }
+  });
+};
 
-// function putIn(employeeClass, input){
-// employeeClass.input = res.name
-// };
-
-
-console.log("it barely works");
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-function teamPrompt() {
+function teamPrompt(status) {
   inquirer.prompt([
     {
       type: "rawlist",
@@ -56,7 +65,7 @@ function teamPrompt() {
     switch (data.employeeType) {
       case "intern":
 
-        const Intern_member = new Intern(data.name, data.id, data.email);
+        let Intern_member = new Intern(data.name, data.id, data.email);
         console.log("Intern: " + JSON.stringify(Intern_member));
 
         inquirer.prompt([
@@ -66,8 +75,9 @@ function teamPrompt() {
             message: "Which school(currently or previously) are you from?"
           }
         ]).then(function (data) {
-
+          console.log("got here with 1");
           Intern_member.school = data.school;
+          status(Intern_member);
           console.log("Intern plus: " + JSON.stringify(Intern_member));
         });
         break;
@@ -83,6 +93,7 @@ function teamPrompt() {
           }
         ]).then(function (data) {
           Engineer_member.github = data.github;
+          status(Engineer_member);
           console.log("Engineer plus: " + JSON.stringify(Engineer_member));
         });
 
@@ -98,76 +109,63 @@ function teamPrompt() {
           }
         ]).then(function (data) {
           Manager_member.officeNumber = data.office_number;
+          status(Manager_member);
           console.log("Manager plus: " + JSON.stringify(Manager_member));
         });
         break;
       default:
     }
+    console.log("got here with 2");
+  });
+};
 
-    // switch (data.employeeType) {
-    //   case "intern":
-    //     inquirer.prompt([
-    //       {
-    //         type: "input",
-    //         name: "school",
-    //         message: "Which school(currently or previously) are you from?"
-    //       }
-    //     ]).then(function (req, res) {
-    //       Intern_member.school = res.school;
-    //       console.log("Intern again: " + JSON.stringify(Intern_member));
-    //     });
-    //     break;
-    //   case "engineer":
-    //     inquirer.prompt([
-    //       {
-    //         type: "input",
-    //         name: "github",
-    //         message: "What is your github username?"
-    //       }
-    //     ]).then(function (req, res) {
-    //       Engineer_member.GitHubUser = res.github;
-    //     }
-    //     );
-    //     break;
-    //   case "manager":
-    //     inquirer.prompt([
-    //       {
-    //         type: "input",
-    //         name: "office_number",
-    //         message: "What is your office number?"
-    //       }
-    //     ]).then(function (req, res) {
-    //       Manager_member.officeNumber = res.office_number;
-    //     }
-    //     );
-    //     break;
-    //   default:
-    // }
+console.log("Welcome to the Team-Profile-Generator!")
+function status_prompt() {
+  inquirer.prompt([
+    {
+      type: "rawlist",
+      message: "Select your choice ",
+      name: "status",
+      choices: [
+        "Starting your team",
+        "Add member to your team",
+        "Return"
+      ]
+    }
+  ]).then(function (data) {
+    switch (data.status) {
+      case "Starting your team":
+        teamPrompt(c_Html);
+        break;
+      case "Add member to your team":
+        teamPrompt(a_Html);
+        break;
+      case "Return":
+        break;
+      default:
+    };
   });
 
-  // console.log("it semi-worked?");
-
-  // After the user has input all employees desired, call the `render` function (required
-  // above) and pass in an array containing all employee objects; the `render` function will
-  // generate and return a block of HTML including templated divs for each employee!
-
-  // After you have your html, you're now ready to create an HTML file using the HTML
-  // returned from the `render` function. Now write it to a file named `team.html` in the
-  // `output` folder. You can use the variable `outputPath` above target this location.
-  // Hint: you may need to check if the `output` folder exists and create it if it
-  // does not.
-
-  // HINT: each employee type (manager, engineer, or intern) has slightly different
-  // information; write your code to ask different questions via inquirer depending on
-  // employee type.
-
-  // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-  // and Intern classes should all extend from a class named Employee; see the directions
-  // for further information. Be sure to test out each class and verify it generates an
-  // object with the correct structure and methods. This structure will be crucial in order
-  // for the provided `render` function to work! ```
-
-
 };
-teamPrompt();
+
+// function continue_prompt() {
+//   inquirer.prompt([
+//     {
+//       type: "confirm",
+//       message: "Do you want to continue adding team members?",
+//       name: "continue"
+
+//     }
+//   ]).then(function (data) {
+//     if (data.continue == true) {
+//       status_prompt();
+//     };
+//   });
+// };
+
+status_prompt();
+//  continue_prompt();
+
+// console.log("Goodbye!");
  // console.log("work please");
+
